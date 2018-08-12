e04e7967cd700cfb42566185471f2593cb7edd95
Sathish Kumar <neosathishgct@gmail.com>
2018-08-13 02:12:32 +0530

    modularisation

---
+++ src/counter.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+    render() {
+        return <h1>Hello Sathish</h1>;
+    }
+}
+
+// export default Counter;
